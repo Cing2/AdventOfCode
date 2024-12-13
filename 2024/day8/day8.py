80ad6653527811d78bf7c750ad84000e48d1b0a1
Camiel Oerlemans <camiel.oerlemans@nxp.com>
2024-12-13 11:25:44 +0100

    day 8

--- 2024/day8/day8.py
+++ 2024/day8/day8.py
@@ -16,12 +16,11 @@ def main():
 
 def part1():
     ant_sets, size_grid = load_input()
-    print(ant_sets, size_grid)
+    # print(ant_sets, size_grid)
 
     distortion_points = set()
     for _, ants in ant_sets.items():
         for combi in itertools.combinations(ants, r=2):
-            print(combi)
             # get all distortion points
             # a+ (a-b) = 2a-b
             dist = (2 * combi[0][0] - combi[1][0], 2 * combi[0][1] - combi[1][1])
@@ -42,24 +41,38 @@ def pos_in_grid(x: int, y: int, height: int, width: int) -> bool:
 
 def part2():
     ant_sets, size_grid = load_input()
-    print(ant_sets, size_grid)
+    # print(ant_sets, size_grid)
 
     distortion_points = set()
     for _, ants in ant_sets.items():
         for combi in itertools.combinations(ants, r=2):
-            print(combi)
-            # get all distortion points
-            # a+ (a-b) = 2a-b
-            dist = (2 * combi[0][0] - combi[1][0], 2 * combi[0][1] - combi[1][1])
-            # check if point on grid
-            if pos_in_grid(dist[0], dist[1], size_grid.height, size_grid.width):
-                distortion_points.add(dist)
-            # b - (a-b) = 2b-a
-            dist = (2 * combi[1][0] - combi[0][0], 2 * combi[1][1] - combi[0][1])
-            if pos_in_grid(dist[0], dist[1], size_grid.height, size_grid.width):
-                distortion_points.add(dist)
+            # distortion on antenna
+            distortion_points.add(combi[0])
+            distortion_points.add(combi[1])
+            # a - b
+            diff = (combi[0][0] - combi[1][0], combi[0][1] - combi[1][1])
+            # direction one, a + (a-b)
+            next_pos = combi[0]
+            while True:
+                next_pos = (next_pos[0] + diff[0], next_pos[1] + diff[1])
+                if pos_in_grid(next_pos[0], next_pos[1], size_grid.height, size_grid.width):
+                    # new distortion point
+                    distortion_points.add(next_pos)
+                else:
+                    # gone of map, stop
+                    break
+            # direction two, b - (a-b)
+            next_pos = combi[1]
+            while True:
+                next_pos = (next_pos[0] - diff[0], next_pos[1] - diff[1])
+                if pos_in_grid(next_pos[0], next_pos[1], size_grid.height, size_grid.width):
+                    # new distortion point
+                    distortion_points.add(next_pos)
+                else:
+                    # gone of map, stop
+                    break
 
-    print("Part 1 =", len(distortion_points))
+    print("Part 2 =", len(distortion_points))
 
 
 Size = namedtuple("size", ("width", "height"))
