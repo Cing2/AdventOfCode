2b84126a4fe6c3b7277ee3be50a26fa697683e0c
camiel.oerlemans <camiel.oerlemans@gmail.com>
2024-12-16 18:32:46 +0100

    day 13

--- 2024/day13/day13.py
+++ 2024/day13/day13.py
@@ -8,29 +8,39 @@ def main():
     part1()
     t_part1 = time.perf_counter()
     print("Time taken =", t_part1 - t_start, "(s)")
-    # part2()
+    part2()
     t_end = time.perf_counter()
     print("Time taken =", t_end - t_part1, "(s)")
 
 
 def part1():
     claw_machines = load_input()
-    # print(claw_machines)
 
     tokens = 0
     for machine in claw_machines:
+        # check if linear independent
+        if machine.get_rank() < 2:
+            print("Not full", machine)
+
         answer = machine.solve()
-        # check if both are round
-        if np.all(np.isclose(answer, answer.astype(int), 0.00001)):
-            print("found solution")
-            print(answer)
-            tokens += int(answer[0]) * 3 + int(answer[1]) * 1
+        tokens += answer
 
     print("Part 1 =", tokens)
 
 
 def part2():
-    sequence = load_input()
+    claw_machines = load_input()
+
+    tokens = 0
+    for machine in claw_machines:
+        # check if linear independent
+        if machine.get_rank() < 2:
+            print("Not full", machine)
+
+        answer = machine.solve(add=np.array([10000000000000, 10000000000000]))
+        tokens += answer
+
+    print("Part 2 =", tokens)
 
 
 class ClawMachine:
@@ -44,13 +54,22 @@ class ClawMachine:
     def get_numpy(self):
         return np.array([self.A, self.B]), np.array(self.prize)
 
-    def solve(self):
+    def get_rank(self):
         X, y = self.get_numpy()
-        return np.linalg.solve(X.T, y)
+        return np.linalg.matrix_rank(X)
+
+    def solve(self, add=np.array([0, 0])):
+        X, y = self.get_numpy()
+        y = y + add
+        answer = np.linalg.solve(X.T, y)
+        answer = answer.round()
+        if (X.T @ answer == y).all():
+            return int((answer @ [3, 1]).sum())
+        return 0
 
 
 def load_input():
-    with open("samples/13.txt") as fp:
+    with open("inputs/13.txt") as fp:
         claw_machines = []
         new_machine = ClawMachine()
         for line in fp.readlines():
