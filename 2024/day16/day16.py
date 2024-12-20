14c6975a76d2ca30ab2be22df843a611f71c7b69
Camiel Oerlemans <camiel.oerlemans@nxp.com>
2024-12-20 12:34:28 +0100

    empty days

---
+++ 2024/day16/day16.py
@@ -0,0 +1,30 @@
+
+import time
+
+def main():
+    print("Day 16")
+    t_start = time.perf_counter()
+    part1()
+    t_part1 = time.perf_counter()
+    print("Time taken =", t_part1-t_start, "(s)")
+    # part2()
+    t_end = time.perf_counter()
+    print("Time taken =", t_end-t_part1, "(s)")
+    
+
+def part1():
+    sequence = load_input()
+    
+
+def part2():
+    sequence = load_input()
+
+
+def load_input():
+    with open("samples/16.txt") as fp:
+        sequence = fp.read()
+    return sequence
+
+
+if __name__ == "__main__":
+    main()
