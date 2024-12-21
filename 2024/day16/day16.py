fb24db1493627f814eec234196cb3c04235c0152
Camiel Oerlemans <camiel.oerlemans@gmail.com>
2024-12-21 15:43:00 +0100

    remove old

--- 2024/day16/day16.py
+++ 2024/day16/day16.py
@@ -47,10 +47,9 @@ class DIR(IntEnum):
 
 
 class Node:
-    def __init__(self, position, dir, parent=None, g=0, h=0):
+    def __init__(self, position, dir, g=0, h=0):
         self.position = position  # (x, y) position
         self.dir = dir
-        self.parents = [parent] if parent is not None else None  # Parent node
         self.g = g  # Cost from start node
         self.h = h  # Heuristic cost to goal
         self.f = g + h  # Total cost (f = g + h)
@@ -158,9 +157,8 @@ def bfs_part2(start, dir, goal, grid):
         cost, froms = visited[node.y][node.x][node.dir]
         for n in froms:
             following.append(n)
-    
+
     return len(all_pos)
-        
 
 
 def get_cheap_neigh(node, grid):
@@ -174,83 +172,6 @@ def get_cheap_neigh(node, grid):
         neigbors.append(CheapNode(new_pos[0], new_pos[1], node.dir, node.cost + 1))
 
     return neigbors
-
-
-def a_star_part2(start, dir, goal, grid):
-    # A* Pathfinding Algorithm
-    open_list: List[Node] = []  # Priority queue for open nodes
-    closed_list = dict()  # Set for closed nodes
-    # nodes that reached the end in lowest g
-    shortest_paths: List[Node] = []
-    length_shortest = math.inf
-
-    # Create the starting node and add it to the open list
-    start_node = Node(start, dir, parent=None, g=0, h=heuristic(start, goal))
-    heapq.heappush(open_list, start_node)
-
-    while open_list:
-        # Get the node with the lowest f value
-        current_node = heapq.heappop(open_list)
-        # print(current_node)
-        if current_node.position == goal:
-            # part 2 needs all shortest paths
-            if len(shortest_paths) == 0:
-                shortest_paths.append(current_node)
-                length_shortest = current_node.g
-            else:
-                if current_node.g == length_shortest:
-                    shortest_paths.append(current_node)
-            continue
-
-        # Add the current node to the closed list
-        closed_list[(current_node.position, current_node.dir)] = current_node
-
-        for neighbor in get_neighbors(current_node, grid, goal, no_parents=True):
-            if neighbor.f > length_shortest:
-                # skip because already have shorter path
-                continue
-            # if a node with the same position as successor is in the closed list which has a
-            # lower f than successor, skip this successor
-            if (neighbor.position, neighbor.dir) in closed_list:
-                if closed_list[(neighbor.position, neighbor.dir)].g < neighbor.g:
-                    continue
-                # if closed_list[(neighbor.position, neighbor.dir)].g == neighbor.g:
-                #     if neighbor.parents is not None:
-                #         closed_list[(neighbor.position, neighbor.dir)].parents.extend(neighbor.parents)
-                #     continue
-
-            # if a node with the same position as  successor  is in the open list which has
-            #   a lower g than successor, skip this successor otherwise, add  the node to the open list
-            in_open = False
-            for node in open_list:
-                if node.position == neighbor.position and node.dir == neighbor.dir:
-                    if node.g < neighbor.g:
-                        # skip this neighbor
-                        in_open = True
-                        break
-                    # elif node.g == neighbor.g:
-                    #     # came to same node with different path
-                    #     if neighbor.parents is not None:
-                    #         node.parents.extend(neighbor.parents)
-                    #     in_open = True
-                    #     break
-
-            if not in_open:
-                heapq.heappush(open_list, neighbor)
-
-    # print(shortest_paths)
-    # calculate nr of nodes in all paths
-    all_pos = set()
-    for node in shortest_paths:
-        to_follow = [node]
-        while len(to_follow) > 0:
-            next = to_follow.pop()
-            all_pos.add(next.position)
-            if next.parents is not None:
-                for parent in next.parents:
-                    to_follow.append(parent)
-
-    return len(all_pos)
 
 
 def heuristic(pos: Tuple[int, int], goal: Tuple[int, int]) -> int:
@@ -299,14 +220,13 @@ def print_grid(grid):
 
 
 # Get valid neighbors of a node in the grid
-def get_neighbors(node: Node, grid: List[List[str]], goal: Tuple[int, int], no_parents=True):
+def get_neighbors(node: Node, grid: List[List[str]], goal: Tuple[int, int]):
     neighbors: List[Node] = []
     # turn 90 clockwise or anti clockwise
     neighbors.append(
         Node(
             node.position,
             dir=turnClockWise(node.dir),
-            parent=node if not no_parents else None,
             g=node.g + 1000,
             h=node.h,
         )
@@ -315,7 +235,6 @@ def get_neighbors(node: Node, grid: List[List[str]], goal: Tuple[int, int], no_p
         Node(
             node.position,
             dir=turnAntiClock(node.dir),
-            parent=node if not no_parents else None,
             g=node.g + 1000,
             h=node.h,
         )
@@ -326,7 +245,7 @@ def get_neighbors(node: Node, grid: List[List[str]], goal: Tuple[int, int], no_p
         pos_in_grid(new_position[0], new_position[1], len(grid), len(grid[0]))
         and grid[new_position[1]][new_position[0]] != "#"
     ):
-        neighbors.append(Node(new_position, dir=node.dir, parent=node if not no_parents else None, g=node.g + 1))
+        neighbors.append(Node(new_position, dir=node.dir, g=node.g + 1))
         neighbors[-1].h = heuristic(neighbors[-1].position, goal)
 
     return neighbors
