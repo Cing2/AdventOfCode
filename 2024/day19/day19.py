705a3fe46c564142753c195e6c57da5faf092553
Camiel Oerlemans <camiel.oerlemans@gmail.com>
2024-12-21 15:49:56 +0100

    empty days

---
+++ 2024/day19/day19.py
@@ -0,0 +1,30 @@
+
+import time
+
+def main():
+    print("Day 19")
+    t_start = time.perf_counter()
+    part1()
+    t_part1 = time.perf_counter()
+    print("Time taken =", t_part1-t_start, "(s)")
+    # part2()
+    t_end = time.perf_counter()
+    print("Time taken =", t_end-t_part1, "(s)")
+    
+
+def part1():
+    sequence = load_input()
+    
+
+def part2():
+    sequence = load_input()
+
+
+def load_input():
+    with open("samples/19.txt") as fp:
+        sequence = fp.read()
+    return sequence
+
+
+if __name__ == "__main__":
+    main()
