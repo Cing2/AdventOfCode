73b80c7bfd482b97c3d4fa9b972a139e8aced08a
Camiel Oerlemans <camiel.oerlemans@nxp.com>
2025-03-11 14:53:35 +0100

    working day 20

--- 2024/day20/day20.py
+++ 2024/day20/day20.py
@@ -7,10 +7,10 @@ from typing import List
 def main():
     print("Day 20")
     t_start = time.perf_counter()
-    # part1()
+    part1()
     t_part1 = time.perf_counter()
     print("Time taken =", t_part1 - t_start, "(s)")
-    part2(True)
+    new_part2()
     t_end = time.perf_counter()
     print("Time taken =", t_end - t_part1, "(s)")
 
@@ -34,7 +34,6 @@ Node = namedtuple("Node", "dim cost")
 
 
 class Counter:
-
     def __init__(self, start: int):
         self.value = start
 
@@ -73,11 +72,8 @@ def distance_to_end(grid, end: Position) -> dict[Position, int]:
 
 
 def part1():
-    grid = load_input()
-    # print(grid)
-
+    grid = load_input(test=False)
     start, end = find_start_end(grid)
-
     dist_to_end = distance_to_end(grid, end)
 
     # bfs over grid with cheat option
@@ -99,7 +95,9 @@ def part1():
 
         if node.dim.id_cheater > 0 and node.dim.pos in dist_to_end:
             # use known distance to calculate cost for cheater
-            new_node = Node(Dim(Position(0, 0), 0), node.cost + dist_to_end[node.dim.pos])
+            new_node = Node(
+                Dim(Position(0, 0), 0), node.cost + dist_to_end[node.dim.pos]
+            )
             finished_cheaters.append(new_node)
             continue
 
@@ -129,7 +127,9 @@ def part1():
     print("Part 1 =", count_100_save)
 
 
-def get_neighbors(node: Node, grid, counter_ids: Counter, cheat_amount: int = 1) -> List[Node]:
+def get_neighbors(
+    node: Node, grid, counter_ids: Counter, cheat_amount: int = 1
+) -> List[Node]:
     neigbors = []
     dirs = [(1, 0), (-1, 0), (0, 1), (0, -1)]
 
@@ -150,12 +150,22 @@ def get_neighbors(node: Node, grid, counter_ids: Counter, cheat_amount: int = 1)
                     )
             else:
                 if grid[new_pos[1]][new_pos[0]] != "#":
-                    neigbors.append(Node(Dim(new_pos, node.dim.id_cheater), node.cost + 1))
+                    neigbors.append(
+                        Node(Dim(new_pos, node.dim.id_cheater), node.cost + 1)
+                    )
                 else:
-                    if node.dim.id_cheater == 0 and counter_ids is not None:  # not cheating, new cheater
+                    if (
+                        node.dim.id_cheater == 0 and counter_ids is not None
+                    ):  # not cheating, new cheater
                         id_cheater = counter_ids.next_value()
                         neigbors.append(
-                            Cheater(Dim(new_pos, id_cheater), node.cost + 1, node.dim.pos, False, cheat_amount)
+                            Cheater(
+                                Dim(new_pos, id_cheater),
+                                node.cost + 1,
+                                node.dim.pos,
+                                False,
+                                cheat_amount,
+                            )
                         )
 
     return neigbors
@@ -168,75 +178,48 @@ def pos_in_grid(x: int, y: int, height: int, width: int) -> bool:
 Cheater = namedtuple("Cheater", "dim cost start_pos prev_block rem_steps")
 
 
-def part2(test=False):
+def new_part2(test=False):
     grid = load_input(test)
     start, end = find_start_end(grid)
     dist_to_end = distance_to_end(grid, end)
+    dist_to_start = distance_to_end(grid, start)
 
-    allowed_cheat = 19
+    allowed_cheat = 20
     min_save = 100
     if test:
         min_save = 50
 
-    # bfs over grid with cheat option
-    visited = set()
-    queue = deque()
-    start = Node(Dim(Position(start[0], start[1]), 0), 0)
-    queue.append(start)
-    visited.add(start)
-    cheater = deque()
-    counter_ids = Counter(0)
-    finished_cheaters = {}
-    min_dist_end = math.inf
-    print(32+31+29+39+25+23+20+19+12+14+12+22+4+3)
+    min_normal_dist = dist_to_end[start]
 
-    while len(queue) > 0 or len(cheater) > 0:
-        if len(queue) > 0:
-            node: Node = queue.popleft()
-        else:
-            node: Cheater = cheater.popleft()
+    # possible_cheats = dict()
+    count_cheats = 0
+    # for every combination of two positions
+    for pos1, distS1 in dist_to_start.items():
+        # # skip position if not possible to save enough
+        # abs_dist_end = abs(pos1.x - end.x) + abs(pos1.y - end.y)
+        # max_savings = min_normal_dist - (distS1 + abs_dist_end)
+        # if max_savings < min_save:
+        #     continue
+        for pos2, distE2 in dist_to_end.items():
+            if pos1 == pos2:
+                continue  # skip same position
+            # check if possible to cheat between, and check if cheat saves anything
+            dist = abs(pos1.x - pos2.x) + abs(pos1.y - pos2.y)
+            if dist <= allowed_cheat:
+                # cost is dist to 1 then dist to 2 and then from 2 to end
+                cost = distS1 + dist + distE2
+                savings = min_normal_dist - cost
+                if savings >= min_save:
+                    count_cheats += 1
+                    # possible_cheats[(pos1, pos2)] = savings
 
-        if node.dim.id_cheater > 0 and node.dim.pos in dist_to_end:
-            # use known distance to calculate cost for cheater
-            key = (node.start_pos, node.dim.pos)
-            if key in finished_cheaters:
-                finished_cheaters[key] = min(finished_cheaters[key], node.cost + dist_to_end[node.dim.pos])
-            else:
-                finished_cheaters[key] = node.cost + dist_to_end[node.dim.pos]
-            # continue if still possible to cheat
-            # if node.rem_steps == 0:
-            #     continue
+    # print(possible_cheats)
+    # count_saved = defaultdict(lambda: 0)
+    # for _, savings in possible_cheats.items():
+    #     count_saved[savings] += 1
+    # print(sorted(list(count_saved.items())))
 
-        if node.dim.pos == end:
-            # at end
-            if node.dim.id_cheater == 0:
-                min_dist_end = node.cost
-            continue
-
-        # get all neighbors
-        for neigh in get_neighbors(node, grid, counter_ids, allowed_cheat):
-            if neigh.dim in visited:
-                continue
-
-            if neigh.dim.id_cheater > 0:
-                cheater.append(neigh)
-            else:
-                queue.append(neigh)
-
-            visited.add(neigh.dim)
-
-    count_100_save = 0
-    count_saved = defaultdict(lambda: 0)
-    for cheater, cost in finished_cheaters.items():
-        diff = min_dist_end - cost
-        if diff >= min_save:
-            count_saved[diff] += 1
-            count_100_save += 1
-        if diff == 76:
-            print(cheater, diff)
-    print(sorted(list(count_saved.items())))
-
-    print("Part 2 =", count_100_save)
+    print("Part 2 =", count_cheats)
 
 
 def load_input(test):
